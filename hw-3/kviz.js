61ed27c902a97d4e0a52cd92322f66b8833c570d
AndrijanaIvkovic <51423090+AndrijanaIvkovic@users.noreply.github.com>
2020-12-22 23:08:59 +0100

    Pravila

--- hw-3/kviz.js
+++ hw-3/kviz.js
@@ -1,3 +1,19 @@
 function onLoad(){
     document.getElementById("name").value = "";
   }
+
+$(document).ready(function(){
+    $("#next").click(function(){
+        var name = $("#name").val().toString();
+        if (name != ""){
+            $(".valid").hide();
+            $("#next").hide();
+            $("#znak").hide();
+            document.getElementById("pocetak").innerHTML = "Upoznajte se sa pravilima, pre nego što započnete kviz.<br>"
+            $("#pocetak").fadeIn();
+            $("#start").fadeIn();
+            $(".pravila").fadeIn();
+        }
+    });
+});
+
