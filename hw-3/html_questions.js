140797f0080ca75e59c1187b011d02a85bdb0c49
AndrijanaIvkovic <51423090+AndrijanaIvkovic@users.noreply.github.com>
2020-12-25 21:36:50 +0100

    ako je korisnik kliknuo na neku opciju, ubacena provera da li je odgovor tacan i ako jeste dodata ikonica za tacno ili netacno ako je odgovor netacan

--- hw-3/html_questions.js
+++ hw-3/html_questions.js
@@ -56,3 +56,38 @@ function showQuetions(index){
         option[i].setAttribute("onclick", "optionSelected(this)");
     }
 }
+let tickIconTag = '<div class="icon tick"><i class="fas fa-check"></i></div>';
+let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div>';
+
+//ako je korisnik kliknuo na neku opciju
+function optionSelected(answer){
+    clearInterval(counter); 
+    clearInterval(counterLine); 
+    let userAns = answer.textContent; 
+    let correcAns = questions[que_count].answer; 
+    const allOptions = option_list.children.length; 
+    
+    if(userAns == correcAns){ //ako je opcija koju je korisnik kliknuo tacan odgovor
+        userScore += 1; //povecaj broj poena za 1
+        answer.classList.add("correct"); //dodaj zelenu boju za tacan odgovor
+        answer.insertAdjacentHTML("beforeend", tickIconTag); //dodaj kvacicu za tacan odgovor
+        console.log("Correct Answer");
+        console.log("Your correct answers = " + userScore);
+    }else{
+        answer.classList.add("incorrect"); //dodaj crvenu boju za netacan odgovor
+        answer.insertAdjacentHTML("beforeend", crossIconTag); //dodaj x ikonicu za netacan odgovor
+        console.log("Wrong Answer");
+
+        for(i=0; i < allOptions; i++){
+            if(option_list.children[i].textContent == correcAns){ 
+                option_list.children[i].setAttribute("class", "option correct"); 
+                option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); 
+                console.log("Auto selected correct answer.");
+            }
+        }
+    }
+    for(i=0; i < allOptions; i++){
+        option_list.children[i].classList.add("disabled"); //kada korisnik jednom izaberu jednu opciju, onemoguci sve ostale
+    }
+    next_btn.classList.add("show"); 
+}
