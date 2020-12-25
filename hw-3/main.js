3c2cd611a67da147bac8b379d6a3e793f8e0e602
AndrijanaIvkovic <51423090+AndrijanaIvkovic@users.noreply.github.com>
2020-12-25 19:28:29 +0100

    Dodata pitanja

---
+++ hw-3/main.js
@@ -0,0 +1,113 @@
+//Pitanja
+let questions = [
+    {
+    numb: 1,
+    question: "U kojoj se državi nalazi Angkor Wat, najveći hramski / religijski kompleks na svetu?",
+    answer: "Kambodža",
+    options: [
+      "Vijetnam",
+      "Peru",
+      "Kambodža",
+      "Tajland"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Koji je film iz 1991. godine osvojio Oskara za najbolji scenario, najbolji film, najboljeg glumca, glumicu i reditelja?",
+    answer: "Kad jaganjci utihnu (eng. The Silence of the Lambs)",
+    options: [
+      "Robin Hood: Princ lopova (eng. Robin Hood: Prince of Thieves)",
+      "Terminator 2: Sudnji dan (eng. Terminator 2: Judgment Day)",
+      "Porodica Adams (eng. The Addams Family)",
+      "Kad jaganjci utihnu (eng. The Silence of the Lambs)"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Koja je reprezentacija pobedila na Svetskom prvenstvu u fudbalu 2010. godine?",
+    answer: "Španija",
+    options: [
+      "Engleska",
+      "Španija",
+      "Portugal",
+      "Italija"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Gde su prvo počele da se koriste papirne novčanice?",
+    answer: "Kina",
+    options: [
+      "Kina",
+      "Indija",
+      "Egipat",
+      "Portugal"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Kako se zvao prvi Zemljin veštački satelit koji je bio lansiran iz SSSRa 1957. godine?",
+    answer: "Sputnjik I",
+    options: [
+      "Vostok",
+      "Lajka",
+      "Apolo 11",
+      "Sputnjik I"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Drugi svetski rat je završen:",
+    answer: "2. septembra 1945. godine",
+    options: [
+      "25. maja 1945. godine",
+      "2. septembra 1945. godine",
+      "5. novembra 1945. godine",
+      "7. oktobra 1945. godine"
+    ]
+  },
+    {
+    numb: 7,
+    question: "Najveća piramida u Egiptu je:",
+    answer: "Keopsova piramida",
+    options: [
+      "Sneferuova piramida",
+      "Kefrenova piramida",
+      "Menkorova piramida",
+      "Keopsova piramida"
+    ]
+  },
+    {
+    numb: 8,
+    question: "Epski roman “Rat i mir” napisao je:",
+    answer: "Lav Nikolajevič Tolstoj",
+    options: [
+      "Fjodor Mihajlovič Dostojevski",
+      "Johann Wolfgang von Goethe",
+      "Lav Nikolajevič Tolstoj",
+      "Sergej Aleksandrovič Jesenjin"
+    ]
+  },
+    {
+    numb: 9,
+    question: "Marie Antoinette (Marija Antoaneta) je bila:",
+    answer: "Francuska kraljica",
+    options: [
+      "Engleska kraljica",
+      "Francuska kraljica",
+      "Operska pevačica",
+      "Glumica"
+    ]
+  },
+    {
+    numb: 10,
+    question: "Ako želite da posetite Baziliku sv. Marka, morate otputovati u:",
+    answer: "Veneciju",
+    options: [
+      "Veneciju",
+      "Madrid",
+      "Barselonu",
+      "Rim"
+    ]
+  },
+];
